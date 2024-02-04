0bab67471c0ff2d0384b3fbea94ced31416c4e5f
developer-hassan <hassanasim5567@gmail.com>
2024-02-04 14:57:51 +0500

    setup shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import { Navigate } from "react-router-dom";
+import { Button } from "@/components/ui/button.tsx";
+import { useNavigate } from "react-router-dom";
 
 function App() {
-  return <Navigate to={"/form/1"} />;
+  const navigate = useNavigate();
+
+  return (
+    <div className="bg-blue-400 w-full h-screen flex items-center justify-center">
+      <Button onClick={() => navigate("/form")}>Go to Form</Button>
+    </div>
+  );
 }
 
 export default App;
