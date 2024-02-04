6df6a1eda15565e11fafa61d081a9dbc4d56df3a
developer-hassan <hassanasim5567@gmail.com>
2024-02-04 15:11:04 +0500

    create generic card

--- src/form/FormCard.tsx
+++ src/form/FormCard.tsx
@@ -1,13 +1,26 @@
 import { Outlet } from "react-router-dom";
+
 import { FormContextProvider } from "./FormContext.tsx";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card.tsx";
 
 export default function FormCard() {
   return (
     <FormContextProvider>
-      <div className={"max-w-lg mx-auto my-5 shadow-xl rounded"}>
-        <p>Hello</p>
-        <Outlet />
-      </div>
+      <Card className={"my-10 mx-auto max-w-lg drop-shadow-md"}>
+        <CardHeader>
+          <CardTitle className={"flex items-center justify-center"}>
+            <img className={"h-auto w-80"} src="/logo.png" alt={"Logo"} />
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Outlet />
+        </CardContent>
+      </Card>
     </FormContextProvider>
   );
 }
